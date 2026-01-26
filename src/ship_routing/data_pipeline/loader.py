a89e857904f864ee60ce69e8f8395442178a62f5
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-01-27 01:29:25 +0530

    Loader class

--- src/ship_routing/data_pipeline/loader.py
+++ src/ship_routing/data_pipeline/loader.py
@@ -15,10 +15,64 @@ class WeatherLoader:
         min_lat, max_lat, min_lon, max_lon = self.Bound
 
         lats = np.arange(min_lat, max_lat, resolution)
-        lons = np.arrage(min_lat, max_lat, resolution)
+        lons = np.arange(min_lon, max_lon, resolution)
 
         times = pd.date_range(start="2026-01-01", period=24, freq="h")
 
         shape = (len(times), len(lats), len(lons))
 
+        u_wind = np.random.normal(0, 5, size=shape) 
+        v_wind = np.random.normal(0,5, size=shape)
+
+        lat_factor = np.sin(np.linspace(0,np.pi, len(lats)))
+        lat_factor = lat_factor.reshape(1,-1,1)
+        u_wind += (5*lat_factor)
+        wind_magnitude = np.sqrt(u_wind**2 + v_wind**2)
+
+        wave_height = 0.2* wind_magnitude + np.random.normal(0,0.5, size=shape)
+        wave_height = np.maximum(wave_height, 0)
+    
+        self.dataset = xr.Dataset(
+            data_vars = {
+                "u_wind": (["time", "lat", "lon"], u_wind),
+                "v_wind": (["time", "lat", "lon"], v_wind),
+                "wave_height" : (['time', 'lat','lon'], wave_height)
+
+            },
+            coords = {
+                'time':times,
+                'lats': lats,
+                'lon': lons
+    
+            },
+            attrs = {
+                'desc': "Synthetic ocean Data",
+                'units': 'm/s for wind, m for waves'
+            }
+        )
+    print('Dataset generated!')
+
+    def save_data(self, filepath):
+        if self.dataset is None : 
+            raise ValueError('No dataset')
+
+        self.dataset.to_netcdf(filepath)
+        print('data saved!')
+
+    def get_conditions(self, lat, lon, time):
+
+        if self.dataset is None:
+            raise ValueError("Dataset not loaded.")
         
+        # "Nearest" interpolation finds the closest grid point to the ship
+        point = self.dataset.sel(
+            lat=lat, 
+            lon=lon, 
+            time=pd.to_datetime(time), 
+            method="nearest"
+        )
+        return {
+            "u_wind": float(point["u_wind"]),
+            "v_wind": float(point["v_wind"]),
+            "wave_height": float(point["wave_height"])
+        }
