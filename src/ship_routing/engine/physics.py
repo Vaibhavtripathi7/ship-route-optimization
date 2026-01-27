34e3f4e99992fff040ffe9dd6afd1ed01fb3b114
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-01-27 17:57:33 +0530

    Wind resistance function

--- src/ship_routing/engine/physics.py
+++ src/ship_routing/engine/physics.py
@@ -27,3 +27,17 @@ class ShipPhysics:
         resistance = 0.5 * self.rho_water * self.surface_water * coeff_of_resist * (speed_knots**2)
         return resistance
 
+    def get_wind_resistance(self, speed_knots, wind_speed):
+        area_front = self.width * 10
+        drag_coeff = 0.8
+        speed_knots = knots_to_ms(speed_knots)
+        wind_speed = knots_to_ms(wind_speed)
+        vel_relative = speed_knots + wind_speed
+        wind_resistance = 0.5 * self.rho_air * drag_coeff * (vel_relative**2) 
+
+        return wind_resistance
+    
+    def calculate_fuel_consumption(self, speed_knots, weather_data: dict[str, float]):
+
+        
+
